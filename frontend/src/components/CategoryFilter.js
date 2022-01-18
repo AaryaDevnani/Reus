47720dabdd4b86b5299612fa7a215e913ceec8e3
amoghagashe07 <73527928+amoghagashe07@users.noreply.github.com>
2022-01-19 04:24:28 +0530

    Modal css changes and Add Items form changes (#7)
    * category filter
    
    * search-bar
    
    * dashboard changes
    
    * nav changes and modal added
    
    * css errors
    
    * Modal css changes and add item form changes
    
    * minor changes

--- frontend/src/components/CategoryFilter.js
+++ frontend/src/components/CategoryFilter.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import "./styles/CategoryItems.css";
 import AddItem from './AddItem';
-import { Modal, Button } from 'react-bootstrap';
+
 
 function CategoryFilter({ categories, onSelectCategory, selectedCategory }) {
-    const [modalShow, setModalShow] = React.useState(false);
+    
     const links = categories.map(i => (
         <div className='categories' key={i.id}>
        
@@ -26,15 +26,7 @@ function CategoryFilter({ categories, onSelectCategory, selectedCategory }) {
         <div className='categoryList'>
             <h2 className='category'>Categories</h2>
             {links}
-            <>
-            <Button className='modal-btn' variant="primary" onClick={() => setModalShow(true)}>
-            Add Item
-            </Button>
-            <AddItem
-            show={modalShow}
-            onHide={() => setModalShow(false)}
-            />
-            </>
+            
         </div>
     )
 }
