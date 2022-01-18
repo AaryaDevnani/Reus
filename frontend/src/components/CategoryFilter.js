9cf2d9d2395455ed07d15d7e138a4e3b41bd1df5
amoghagashe07 <73527928+amoghagashe07@users.noreply.github.com>
2022-01-19 02:33:01 +0530

    Nav changes and Added modal (#5)
    * category filter
    
    * search-bar
    
    * dashboard changes
    
    * nav changes and modal added
    
    * css errors

--- frontend/src/components/CategoryFilter.js
+++ frontend/src/components/CategoryFilter.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import "./styles/CategoryItems.css";
+import AddItem from './AddItem';
+import { Modal, Button } from 'react-bootstrap';
 
 function CategoryFilter({ categories, onSelectCategory, selectedCategory }) {
+    const [modalShow, setModalShow] = React.useState(false);
     const links = categories.map(i => (
         <div className='categories' key={i.id}>
        
@@ -23,6 +26,15 @@ function CategoryFilter({ categories, onSelectCategory, selectedCategory }) {
         <div className='categoryList'>
             <h2 className='category'>Categories</h2>
             {links}
+            <>
+            <Button className='modal-btn' variant="primary" onClick={() => setModalShow(true)}>
+            Add Item
+            </Button>
+            <AddItem
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+            />
+            </>
         </div>
     )
 }
