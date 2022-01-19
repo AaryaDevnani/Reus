55ad7f41d7c6d3031d03bf4fe0ac7aed1d1cad5d
KavishShah09 <kavishshah09@gmail.com>
2022-01-19 17:37:18 +0530

    shopping list css changes

--- frontend/src/components/ShoppingList.js
+++ frontend/src/components/ShoppingList.js
@@ -1,77 +1,73 @@
-import React, {useState, useEffect} from 'react';
-import { useHistory, useLocation , NavLink} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useHistory, useLocation, NavLink } from 'react-router-dom';
 import { Button, CardGroup, Card, Container, Row, Col } from 'react-bootstrap';
-import "./styles/ShoppingList.css";
+import './styles/ShoppingList.css';
 import { QuantityPicker } from 'react-qty-picker';
 import { FaTrash } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
-import  AddShoppingItem  from './AddShoppingItem';
-
+import AddShoppingItem from './AddShoppingItem';
 
 function ShoppingList() {
-    const [modalShow, setModalShow] = React.useState(false);
-        const { userId } = useSelector((state) => state.auth);
-        const [items, setItems] = useState([]);
-        const getGroceries = async () => {
-          const response = await fetch(`/api/groceries`, {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-              UserId: userId
-            }
-          });
-          const data = await response.json();
-          if (!data.error === '') return data.error;
-          else {
-            setItems(data.groceryItems);
-            console.log(data)
-          }
-        };
+  const [modalShow, setModalShow] = React.useState(false);
+  const { userId } = useSelector((state) => state.auth);
+  const [items, setItems] = useState([]);
+  const getGroceries = async () => {
+    const response = await fetch(`/api/groceries`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        UserId: userId
+      }
+    });
+    const data = await response.json();
+    if (!data.error === '') return data.error;
+    else {
+      setItems(data.groceryItems);
+      console.log(data);
+    }
+  };
 
-        useEffect(() => {
-            getGroceries();
-          }, []);
+  useEffect(() => {
+    getGroceries();
+  }, []);
 
+  return (
+    <div className="dashboardPage">
+      <p className="intro">Your Shopping List</p>
+      <>
+        <Button
+          className="butt"
+          variant="primary"
+          onClick={() => setModalShow(true)}
+        >
+          Add Item
+        </Button>
 
-          
-    return (
-        <div className="dashboardPage">
-            <p className='intro'>Your Shopping List</p>
-            <>
-      <Button className='butt' variant="primary" onClick={() => setModalShow(true)}>
-        Add Item
-      </Button>
+        <AddShoppingItem show={modalShow} onHide={() => setModalShow(false)} />
+      </>
+      <div className="list-items">
+        <div>
+          <div className="list-item1">
+            <h3 className="item1">Item</h3>
+            <h3 className="quantity">Quantity</h3>
+            <h3 className="remove">Remove</h3>
+          </div>
 
-      <AddShoppingItem
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-      />
-    </>
-            <div className='list-items'> 
-            <div className='list-item1'>
-                <div>
-                <h3 className='item1'>Item</h3> 
-                <h3 className='quantity'>Quantity</h3> 
-                <h3 className='remove'>Remove</h3> 
-                </div>
+          {items.map((item) => (
+            <div className="row list-item">
+              <h3 className="col name">{item.name}</h3>
+              <p className="col qty">
+                <QuantityPicker min={1} smooth width="8rem" />
+              </p>
+              <button type="button" className="bin col">
+                <FaTrash />
+              </button>
             </div>
-        
-            
-                
-                    {items.map((item) => (
-                        <div className='list-item'>
-                        <div>
-                        <h3 className='flashName'>{item.name}</h3> 
-                        <p className='flashData'><QuantityPicker min={1} smooth/></p>
-                        <Button variant='outline-dark' className='btn1'>Delete </Button>
-                        </div>
-                        </div>
-                      ))}
-            
-            
-            </div> 
+          ))}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default ShoppingList;
