31f8f355603a18e4a34aae2882810274ea4fd6b5
pri <prishapanchmia@gmail.com>
2022-01-20 01:42:05 +0530

    home page changes

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -17,63 +17,63 @@ function Home() {
                 <img src={reduce} className='reduce'></img>
                         <p className='header'>READY TO JOIN THE BATTLE AGAINST FOOD WASTE?</p>
                 </Container> */}
+                
                 <div className='reduce'>
           <img src={reduce} className='ig' alt="donate" width="300"  />
+          
           <div >
            <h1 className='bannerReduce'>READY TO JOIN THE BATTLE AGAINST FOOD WASTE?</h1>
           </div>
-            </div>
-        <Container >
+          </div>
+        <div className='ban' >
                 <Row>
-                        <Col sm={6}>
-                                <img className='image' src={home_image} alt="" />
-                        </Col>
-
-                        <Col sm={6}>
+                <Col sm={6}>
                         <h5 className='text'>World Hunger is on the rise; yet, an estimated 1/3 of all food 
                         produced globally is lost or goes to waste. We all have a part to play in reducing 
                         food loss and waste, not only for the sake of the food but for the resources that go into it.
                         </h5>
                         </Col>
+                        <Col sm={6}>
+                                <img className='image' src={home_image} alt="" />
+                        </Col>
+
+                        
                 </Row>
-        </Container>
+        </div>
 
         <Container>
                 <Row>
+                <Col sm={6}>
+                                <img className='image2' src={home_img5} alt="" />
+                        </Col>
                         <Col sm={6}>
-                                <h5 className='text'> As per the Food Waste Index Report (2021) of United Nations Environment Programme, 
+                                <h5 className='text2'> As per the Food Waste Index Report (2021) of United Nations Environment Programme, 
                                 a staggering 50 kg of food is thrown away per person every year in Indian homes. 
                                 This excess food waste usually ends up in landfills, creating potent greenhouse gases 
                                 which have dire environmental implications. Meanwhile, we continue to be greenwashed into 
                                 amassing more “organic” and “sustainable” products than we really need.</h5>
                         </Col>
 
-                        <Col sm={6}>
-                                <img className='image2' src={home_img5} alt="" />
-                        </Col>
+                        
                 </Row>
         </Container>
 
-       
 
-        <Container className='text2'>
-                <Row>
+        
+        <p className='heh'>Here's how you can make a difference!</p>
+
+        <Container className='text3'>
                         <Col>
-                                <h5 className='text'>Many people are prone to dismissing food waste as 
+                                <h5 >Many people are prone to dismissing food waste as 
                                 someone else's issue ("I don't waste any food") or focusing primarily on the 
                                 more overtly distressing examples of waste (unharvested fields of produce 
                                 ploughed back into the earth, supermarket skip waste). The reality is that 
                                 more than half of all food waste in the 'civilised' globe occurs in our households.
                                 So, the bad news is we are half the problem. But the good news is… we can be half the solution!</h5>
                         </Col>
-
-                </Row>
         </Container>
-        
-        <p className='heh'>Here's how you can make a difference!</p>
 
         <Container className='text'>
-                <Row>
                         <Col >
                                 <h5 className='text3'>Easily keep track of, organise, and manage your household's food!
                                                 You can simply check what food you have left in your freezer, fridge, and pantry, 
@@ -81,11 +81,9 @@ function Home() {
                                                 avoid wasteful purchases, reduce food waste, and save a lot of money using lists for 
                                                 your freezer, fridge, and pantry.</h5>
                         </Col>
-
-                </Row>
         </Container>
 
-        <p className='heh'>Key Features!</p>
+        <p className='heh2'>Key Features!</p>
         <Container>
                 <Row>
                         <Col sm={6}>
@@ -93,7 +91,7 @@ function Home() {
                         </Col>
 
                         <Col sm={6}>
-                        <h5 className='text'>Track your food and keep the expiration dates in check</h5>
+                        <h5 className='text2'>Track your food and keep the expiration dates in check</h5>
                         </Col>
                 </Row>
         </Container>
@@ -101,7 +99,7 @@ function Home() {
         <Container>
                 <Row>
                         <Col sm={6}>
-                                <h5 className='text'> 
+                                <h5 className='text2'> 
                                 Make a conscious effort to reduce food waste
                                 </h5>
                         </Col>
