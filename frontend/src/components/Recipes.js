ac8d29ebe0dd531304a9a2b6edbc5c85f63978f4
KavishShah09 <kavishshah09@gmail.com>
2022-01-20 05:59:55 +0530

    fixed search

--- frontend/src/components/Recipes.js
+++ frontend/src/components/Recipes.js
@@ -12,7 +12,7 @@ function Recipes() {
   const [recipes, setRecipes] = useState([]);
   const [byproducts, setByproducts] = useState([]);
   const [ingredients, setIngredients] = useState([]);
-  const [adding,setAdding]=useState(['Broccoli']);
+  const [adding, setAdding] = useState(['Broccoli']);
   const getIngredients = async () => {
     const response = await fetch(`/api/items`, {
       method: 'GET',
@@ -27,34 +27,36 @@ function Recipes() {
       setIngredients(data.items);
     }
   };
-  const getByproducts = async()=>{
-    const response = await fetch(`api/byproduct`,{
+  const getByproducts = async () => {
+    const response = await fetch(`api/byproduct`, {
       method: 'GET',
       headers: {
-        'Content-Type': 'application/json',
-      }})
-      const data = await response.json();
-      if (!data.error === '') return data.error;
-      else {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    if (!data.error === '') return data.error;
+    else {
       setByproducts(data.items);
     }
-    }
+  };
 
-  const getAdding = (name)=>{
-    if(adding.includes(name)){
-      setAdding(adding.filter(a => a !== name))
-      setSearchFinal(adding.join(" "))
-    }else{
-      setAdding([...adding,name])
-      setSearchFinal(searchFinal + " " + name)
+  const getAdding = (name) => {
+    if (adding.includes(name)) {
+      setAdding(adding.filter((a) => a !== name));
+      setSearchFinal(adding.filter((a) => a !== name).join(' '));
+      setSearch(adding.filter((a) => a !== name).join(' '));
+    } else {
+      setAdding([...adding, name]);
+      setSearchFinal(searchFinal + ' ' + name);
+      setSearch(searchFinal + ' ' + name);
     }
-  }
+  };
 
   const getRecipes = async () => {
     const appID = 'a670aefe';
     const app_key = '2062231e1e23e9cfc408fa3516285253d8';
     const apiURL = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchFinal}&app_id=${appID}&app_key=%${app_key}`;
-    // const apiURL = `https://api.edamam.com/api/recipes/v2/4442d46778d89c3b9adf19580b3fd317?type=public&app_id=a670aefe&app_key=2062231e1e23e9cfc408fa3516285253d8
 
     try {
       const response = await fetch(apiURL, {
@@ -66,8 +68,8 @@ function Recipes() {
       const data = await response.json();
       setRecipes(data.hits);
     } catch (error) {
-      console.log({error});
-      alert( error.message);
+      console.log({ error });
+      alert(error.message);
     }
   };
 
@@ -83,58 +85,67 @@ function Recipes() {
     getRecipes();
     getIngredients();
     getByproducts();
-    
   }, [searchFinal]);
   return (
     <div className="recipePage container">
-      
       <div className="row">
         <div className="col-5 ingre-by">
-        <div className="input-group">
-        <div className="form-outline">
-          <input
-            type="search"
-            id="form1"
-            className="form-control"
-            value={search}
-            onChange={handleOnChange}
-          />
-          
-        </div>
-        <div className="search-btn">
-          <button
-            type="button"
-            className="btn btn-primary search"
-            onClick={handleOnClick}
-          >
-            <FaSearch />
-          </button>
-        </div>
-      </div>
-          <p className='ingr' >Ingredients</p>
+          <div className="input-group">
+            <div className="form-outline">
+              <input
+                type="search"
+                id="form1"
+                className="form-control"
+                value={search}
+                onChange={handleOnChange}
+              />
+            </div>
+            <div className="search-btn">
+              <button
+                type="button"
+                className="btn btn-primary search"
+                onClick={handleOnClick}
+              >
+                <FaSearch />
+              </button>
+            </div>
+          </div>
+          <p className="ingr">Ingredients</p>
           <div className="invItems">
             <div className="btns">
               {ingredients.map((ingredient) => (
-                  <input
-                    key={ingredient._id}
-                    type="button"
-                    className={adding.includes(ingredient.name) ? "butn pill pill-active" : "butn pill pill-inactive"}
-                    value={ingredient.name}
-                    onClick={()=>{getAdding(ingredient.name)}}
-                  />
+                <input
+                  key={ingredient._id}
+                  type="button"
+                  className={
+                    adding.includes(ingredient.name)
+                      ? 'butn pill pill-active'
+                      : 'butn pill pill-inactive'
+                  }
+                  value={ingredient.name}
+                  onClick={() => {
+                    getAdding(ingredient.name);
+                  }}
+                />
               ))}
             </div>
-          </div>  
-          <p className='byproduct' >Byproducts</p> 
-          <div className='invItems'>
-          <Recipebyproduct ingredients={ingredients} byproducts={byproducts} />
+          </div>
+          <p className="byproduct">Byproducts</p>
+          <div className="invItems">
+            <Recipebyproduct
+              ingredients={ingredients}
+              byproducts={byproducts}
+            />
           </div>
         </div>
         <div className="col-5 listItems recip-list">
-          <p className='recp' >Recipes</p>
+          <p className="recp">Recipes</p>
           {recipes.map(({ recipe }) => (
             <div className="item">
-              <p className="recipeName" dangerouslySetInnerHTML={{__html: recipe.label}}></p>
+              <p
+                className="recipeName"
+                dangerouslySetInnerHTML={{ __html: recipe.label }}
+              ></p>
               <div className="recipeCard">
                 {recipe.images.THUMBNAIL ? (
                   <img
@@ -156,22 +167,33 @@ function Recipes() {
                     <li>{i.text}</li>
                   ))}
                 </ul> */}
-                <ul className='recipeText'>
-                {recipe.dietLabels && recipe.dietLabels.length>0 ? (
-                    <li>Diet Labels: {recipe.dietLabels.join(", ")}</li>
-                ):<div></div>}
-                {recipe.cuisineType ? (
-                  <li>Cuisine: {recipe.cuisineType}</li>
-                ):<div></div>}
-                {recipe.dishType ? (
-                  <li>Dish Type: {recipe.dishType}</li>
-                ):<div></div>}
-                {recipe.mealType ? (
-                  <li>Meal Type: {recipe.mealType}</li>
-                ):<div></div>}
-                {<li>Calories: {recipe.calories.toPrecision(4)}</li>}
+                <ul className="recipeText">
+                  {recipe.dietLabels && recipe.dietLabels.length > 0 ? (
+                    <li>Diet Labels: {recipe.dietLabels.join(', ')}</li>
+                  ) : (
+                    <div></div>
+                  )}
+                  {recipe.cuisineType ? (
+                    <li>Cuisine: {recipe.cuisineType}</li>
+                  ) : (
+                    <div></div>
+                  )}
+                  {recipe.dishType ? (
+                    <li>Dish Type: {recipe.dishType}</li>
+                  ) : (
+                    <div></div>
+                  )}
+                  {recipe.mealType ? (
+                    <li>Meal Type: {recipe.mealType}</li>
+                  ) : (
+                    <div></div>
+                  )}
+                  {<li>Calories: {recipe.calories.toPrecision(4)}</li>}
                 </ul>
-                <Link to={`/recipe/${recipe.uri.split("_")[1]}`} className="recipeButton" >
+                <Link
+                  to={`/recipe/${recipe.uri.split('_')[1]}`}
+                  className="recipeButton"
+                >
                   &gt;
                 </Link>
               </div>
